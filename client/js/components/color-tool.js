c706f216aa0d707ec3309c60b58fb686d83c36b3
Eric W. Greene <eric@ericwgreene.com>
2017-10-06 15:28:32 -0400

    added form

--- client/js/components/color-tool.js
+++ client/js/components/color-tool.js
@@ -3,8 +3,24 @@ import * as React from 'react';
 // named export
 export class ColorTool extends React.Component {
 
-  render() {
+  constructor(props) {
+    super(props);
 
+    this.state = {
+      newColorName: '',
+      newColorHexCode: '',
+    };
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      [ e.target.name ]: e.target.value,
+    });
+  }
+
+  render() {
     return <div>
       <header>
         <h1>Color Tool</h1>
@@ -12,6 +28,18 @@ export class ColorTool extends React.Component {
       <ul>
         {this.props.colors.map(color => <li key={color.id}>{color.name}</li>)}
       </ul>
+      <form>
+        <div>
+          <label htmlFor="new-color-name-input">New Color Name:</label>
+          <input type="text" id="new-color-name-input" name="newColorName"
+            value={this.state.newColorName} onChange={this.onChange} />
+        </div>
+        <div>
+          <label htmlFor="new-color-name-input">New Color HexCode:</label>
+          <input type="text" id="new-color-name-input" name="newColorName"
+            value={this.state.newColorName} onChange={this.onChange} />
+        </div>
+      </form>
     </div>;
   }
 
