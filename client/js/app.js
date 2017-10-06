383b23a70413295839b29b0f0bafa8d0f19ff97f
Eric W. Greene <eric@ericwgreene.com>
2017-10-06 14:49:44 -0400

    added code demos

--- client/js/app.js
+++ client/js/app.js
@@ -4,5 +4,10 @@ import * as ReactDOM from 'react-dom';
 // named import
 import { ColorTool } from './components/color-tool';
 
+const myColors = [
+  { id: 1, name: 'red', hexCode: '#ff0000' },
+  { id: 2, name: 'white', hexCode: '#ffffff' },
+  { id: 3, name: 'blue', hexCode: '#0000ff' },
+];
 
-ReactDOM.render(React.createElement(ColorTool), document.querySelector('main'));
+ReactDOM.render(<ColorTool colors={myColors} />, document.querySelector('main'));
--- client/js/components/color-tool.js
+++ client/js/components/color-tool.js
@@ -5,18 +5,12 @@ export class ColorTool extends React.Component {
 
   render() {
 
-    const colors = [
-      { id: 1, name: 'red', hexCode: '#ff0000' },
-      { id: 2, name: 'white', hexCode: '#ff0000' },
-      { id: 3, name: 'blue', hexCode: '#ff0000' },
-    ];
-
     return <div>
       <header>
         <h1>Color Tool</h1>
       </header>
       <ul>
-        {colors.map(color => <li key={color.id}>{color.name}</li>)}
+        {this.props.colors.map(color => <li key={color.id}>{color.name}</li>)}
       </ul>
     </div>;
   }
