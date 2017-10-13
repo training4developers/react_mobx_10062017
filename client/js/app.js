fd1ed9f5d8a5aeea3edf7168d593bf5d8129059a
Eric W. Greene <eric@ericwgreene.com>
2017-10-13 13:59:17 -0400

    added code demos

--- client/js/app.js
+++ client/js/app.js
@@ -4,7 +4,7 @@ import * as ReactDOM from 'react-dom';
 import { CarStore } from './stores/car-store';
 import { CarTool } from './components/car-tool';
 
-ReactDOM.render(
-  <CarTool store={new CarStore} />,
-  document.querySelector('main')
-);
+// ReactDOM.render(
+//   <CarTool store={new CarStore} />,
+//   document.querySelector('main')
+// );
