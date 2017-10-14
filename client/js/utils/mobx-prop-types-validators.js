72048fc706cd3c4def915cf5065c1353ef7c63e9
Eric W. Greene <eric@ericwgreene.com>
2017-10-14 13:24:49 -0400

    added comments to the mobx prop types and tests

--- client/js/utils/mobx-prop-types-validators.js
+++ client/js/utils/mobx-prop-types-validators.js
@@ -1,43 +1,62 @@
 import { isBoxedObservable } from 'mobx';
 import { PropTypes as StandardMobxPropTypes } from 'mobx-react';
 
+// intended to replace the PropTypes from 'mobx-react';
+// freezing the exported object to prevent changes to it
 export const MobxPropTypes = Object.freeze(new class {
 
   constructor() {
+    // the original PropTypes from 'mobx-react' is frozen
+    // copy the PropTypes to this replacement version
     Object.assign(this, StandardMobxPropTypes);
+
+    // configure PropTypes for observable values of primitive types
     this.observableValue.isRequired = this.createIsRequired(this.observableValue);
     this.observableNumber = this.createObservableValueSpecificType('number', true);
     this.observableString = this.createObservableValueSpecificType('string', true);
     this.observableBoolean = this.createObservableValueSpecificType('boolean', true);
   }
 
+  // default PropType for an ObservableValue
   observableValue(props, propName, componentName) {
 
+    // MobX provides a low level function to check if a value is boxed primitive value
     return !isBoxedObservable(props[propName]) &&
       new Error(`Invalid prop \`${propName}\` of type \`${typeof props[propName]}\` supplied to \`${componentName}\`, expected an \`observable\`.`);
   }
 
-  createIsRequired(typeValidatorFn) {
+  // provides the isRequired function will be chained to the end of the other PropTypes
+  // the param validatorFn is the PropType validation function to which the isRequired
+  // function will be chained
+  createIsRequired(validatorFn) {
     
     return (props, propName, componentName) =>
+      // only checking to see if the props has the property
+      // if props has the property, but's it undefined it will be caught
+      // by the validatorFn
       !props.hasOwnProperty(propName)
         ? new Error(`Prop \`${propName}\` is marked required in \`${componentName}\`, but its value is \`${props[propName]}\`.`)
-        : typeValidatorFn(props, propName, componentName);
+        : validatorFn(props, propName, componentName);
   }
 
+  // creates PropTypes for specific kind of ObservableValue such as number or string
   createObservableValueSpecificType(typeName, supportIsRequired = true) {
 
+    // return the validator function for the specific observable primitive type
     const validatorFn = (props, propName, componentName) => {
       
+      // check if the props is a boxed obserable value
       const err = this.observableValue(props, propName, componentName);
       if (err) { return err; }
 
+      // check the type of the unboxed value
       if (typeof props[propName].get() !== typeName) {
         return new Error(`Invalid prop \`${propName}\` of type \`${typeof props[propName]}\` supplied to \`${componentName}\`, expected an \`observable\` of type \`${typeName}\`.`);
       }
 
     };
 
+    // true if a chained isRequired function should be added
     supportIsRequired && (validatorFn.isRequired = this.createIsRequired(validatorFn));
 
     return validatorFn;
--- client/js/utils/mobx-prop-types-validators.test.js
+++ client/js/utils/mobx-prop-types-validators.test.js
@@ -4,36 +4,57 @@ import { observable } from 'mobx';
 
 import { MobxPropTypes } from './mobx-prop-types-validators';
 
+// sets up a test suite
 describe('Obserable Value PropTypes', () => {
 
+  // variable for the component which will be used for each test
   let TestComponent;
 
+  // will run before each test in the test suite
   beforeEach(() => {
+    // reinitialize the component so the proptypes form the previous test
+    // do not hang around
+    // each test should run with its own environment
     TestComponent = () => null;
   });
 
+  // nested test suites allow for beforeEach and afterEach functions
+  // which only apply to some of the tests
   describe('Observable Value Only', () => {
 
+    // will be used to spy on the console error function to look
+    // for proptypes failures when testing the proptypes validation
     let consoleErrorSpy;
 
     beforeEach(() => {
 
+      // setup spy on error function of console object
       consoleErrorSpy = jest.spyOn(console, 'error');
 
+      // setup proptypes for this suite's tests
       TestComponent.propTypes = {
         test: MobxPropTypes.observableValue,
       };
     });
 
+    // will run after each and every test in the suite
     afterEach(() => {
+      // the spy on console.error must be cleaned up
       consoleErrorSpy.mockRestore();
     });
 
+    // the test function defines a test
     test('Observable Value prop type fails', () => {
       
       const test = 'test';
-  
+      
+      // using shallow rendering because it is faster the DOM rendering and
+      // DOM testing is not needed
       shallow(<TestComponent test={test} />);
+
+      // Jest uses the expect flavor assertions
+      // checking to make sure the spied upon function was called
+      // with the proper arguments
       expect(consoleErrorSpy).toHaveBeenCalledWith('Warning: Failed prop type: Invalid prop `test` of type `string` supplied to `TestComponent`, expected an `observable`.\n    in TestComponent');
     });
   
